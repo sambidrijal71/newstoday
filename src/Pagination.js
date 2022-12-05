65074a847768b851a55b533e14e0dfe68bd005ee
srijal <srijal@planittesting.com>
2022-12-05 18:45:46 +1000

    added pagination feature

--- src/Pagination.js
+++ src/Pagination.js
@@ -2,13 +2,14 @@ import React from 'react'
 import { useGlobalContext } from './Context'
 
 const Pagination = () => {
-    const { page, nbPages } = useGlobalContext();
+    const { page, nbPages, getPrevPage, getNextPage } = useGlobalContext();
     return (
         <>
-            <div className="pagination_btn"></div>
-            {/* <button onClick={() => getPrevPage()}>Previous</button> */}
-            <p>{page} of {nbPages} </p>
-            {/* <button onClick={() => getNextPage()}>Next</button> */}
+            <div className="pagination-btn">
+                <button onClick={() => getPrevPage()}>Previous</button>
+                <p>{page + 1} of {nbPages} </p>
+                <button onClick={() => getNextPage()}>Next</button>
+            </div>
         </>
     )
 }
--- src/Stories.js
+++ src/Stories.js
@@ -4,31 +4,36 @@ import { useGlobalContext } from "./Context";
 const Stories = () => {
     const { hits, isLoading, removePost } = useGlobalContext();
     if (isLoading) {
-        return (<>
-            <h1>Loading...</h1>
-
-        </>)
+        return (
+            <>
+                <h1>Loading...</h1>
+            </>
+        );
     }
     return (
-        <>
-            <div className="stories-div">
-                {hits.map((curPost) => {
-                    const { title, author, objectID, url, num_comments } = curPost;
-                    return <>
-                        <div className="card" key={objectID}>
-                            <h2>{title}</h2>
-                            <p>
-                                <span>Posted By {author}</span> | <span>{num_comments}</span> comments
-                            </p>
-                            <div className="card-button">
-                                <a href={url} rel="noreferrer" target="_blank">Read More</a>
-                                <a href="#" onClick={() => removePost(objectID)} rel="noreferrer">Remove</a>
-                            </div>
+        <div className="stories-div">
+            {hits.map((curPost) => {
+                const { title, author, objectID, url, num_comments } = curPost;
+                return (
+                    <div className="card" key={objectID}>
+                        <h2>{title}</h2>
+                        <p>
+                            <span>Posted By {author}</span> | <span>{num_comments}</span>{" "}
+                            comments
+                        </p>
+                        <div className="card-button">
+                            <a href={url} rel="noreferrer" target="_blank">
+                                Read More
+                            </a>
+                            <a href="#" onClick={() => removePost(objectID)} rel="noreferrer">
+                                Remove
+                            </a>
                         </div>
-                    </>
-                })}</div></>
-    )
+                    </div>
+                );
+            })}
+        </div>
+    );
+};
 
-}
-
-export default Stories
+export default Stories;
